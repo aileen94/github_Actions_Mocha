f960fde19cd5afd53639ad000c01335fb7152f28
Ewering <Aileen.Ewering@otto.de>
2020-10-19 11:56:44 +0200

    add new testfile

---
+++ test/otherTest.js
@@ -0,0 +1,10 @@
+let convert = require('../app.js')
+let assert = require('assert');
+var should = require('mocha-should')
+var result=40
+describe('Temperature Conversion', function() {
+
+   it('should return 40', function() {
+          assert.equal(40, result);
+      });
+});
